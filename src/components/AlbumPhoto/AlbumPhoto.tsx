a8e26f9007bb8b5d40b213a645a82b2cff4dc784
itamar80 <itti37@gmail.com>
2022-11-30 00:42:15 +0200

    Add thumbnail title by hover

--- src/components/AlbumPhoto/AlbumPhoto.tsx
+++ src/components/AlbumPhoto/AlbumPhoto.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { PhotoDTO } from '../../types/types';
 import './AlbumPhoto.css'
 
@@ -9,8 +9,15 @@ type Props = {
 
 
 export const AlbumPhoto: FC<Props> = ({ photo, deletePhoto }): JSX.Element => {
+    const [isHover, setIsHover] = useState<boolean>(false);
     return (
-        <div className='photo-container'>
+        <div className='photo-container'
+            onMouseEnter={() => setIsHover(true)}
+            onMouseLeave={() => setIsHover(false)}
+        >
+            {isHover && <div className='thumbnail'>
+                {photo.title}
+            </div>}
             <button className='exit-button' onClick={() => deletePhoto(photo.id)}>x</button>
             <img src={photo.thumbnailUrl} alt={'thumbnail'} />
         </div>
