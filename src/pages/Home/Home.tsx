420d22a45084f76a7f7def28c749cdb8da9864aa
itamar80 <itti37@gmail.com>
2022-11-29 19:17:46 +0200

    Initiat router

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from 'react';
+
+type Props = {
+
+}
+
+export const Home: FC<Props> = (): JSX.Element => {
+    return (
+        <div>home</div>
+    )
+}
---
+++ src/pages/Home/NotHome.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const NotHome = () => {
+    return (
+        <div>not home</div>
+    )
+}
