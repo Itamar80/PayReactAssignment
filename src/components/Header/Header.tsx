4106e33220cb2f340a8ac332c50ead0181b3fa01
itamar80 <itti37@gmail.com>
2022-12-01 16:21:06 +0200

    Delete unused props

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,7 @@
 import { FC } from 'react';
 import './Header.css'
 
-type Props = {
-}
-
-
-export const Header: FC<Props> = (): JSX.Element => {
+export const Header: FC = (): JSX.Element => {
     return (
         <header className='header-container'>
             <h1>Pay.</h1>
--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,11 +1,7 @@
 import { FC } from 'react';
 import './Loader.css'
 
-type Props = {
-}
-
-
-export const Loader: FC<Props> = (): JSX.Element => {
+export const Loader: FC = (): JSX.Element => {
     return (
         <div className='loader-container'>
             <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
